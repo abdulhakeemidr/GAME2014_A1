b0fddd793fdd6843b0340cc1e0cb1484881cced9
abdulhakeemidr <56049093+abdulhakeemidr@users.noreply.github.com>
2021-10-23 13:58:43 -0400

    Created Player Animation and Joystick for Movement

--- A1_GAME2014/Assets/[Scripts]/PlayerController.cs
+++ A1_GAME2014/Assets/[Scripts]/PlayerController.cs
@@ -7,16 +7,81 @@ public class PlayerController : MonoBehaviour
     private Vector2 moveDir;
     private Rigidbody2D playerRb;
     public int moveSpeed = 5;
-
+    private Animator animator;
+    public Joystick joystick;
+    int MoveRightAnim;
+    int MoveLeftAnim;
+    int MoveUpAnim;
 
     void Start()
     {
         playerRb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+
+        MoveRightAnim = Animator.StringToHash("isMovingRight");
+        MoveLeftAnim = Animator.StringToHash("isMovingLeft");
+        MoveUpAnim = Animator.StringToHash("isMovingUp");
     }
 
     void Update()
     {
-        moveDir = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        
+        MoveTouchInput();
+        MoveKeyInput();
+        AnimateMovement();
         playerRb.MovePosition((Vector2)transform.position + (moveSpeed * moveDir * Time.deltaTime));
+    }
+
+    void MoveTouchInput()
+    {
+        moveDir = new Vector2(joystick.Horizontal, joystick.Vertical);
+        Debug.Log(moveDir.x + ", " + moveDir.y);
+    }
+
+    void MoveKeyInput()
+    {
+        if(Input.anyKeyDown)
+        {
+            moveDir = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        }
+    }
+
+    void AnimateMovement()
+    {
+        // For Moving Left
+        if(moveDir.x < 0)
+        {
+            animator.SetBool(MoveRightAnim, false);
+            animator.SetBool(MoveUpAnim, false);
+            animator.SetBool(MoveLeftAnim, true);
+        }
+        else if(moveDir.x == 0)
+        {
+            animator.SetBool(MoveLeftAnim, false);
+        }
+
+        // For Moving Right
+        if (moveDir.x > 0)
+        {
+            animator.SetBool(MoveLeftAnim, false);
+            animator.SetBool(MoveUpAnim, false);
+            animator.SetBool(MoveRightAnim, true);
+        }
+        else if (moveDir.x == 0)
+        {
+            animator.SetBool(MoveRightAnim, false);
+        }
+
+        // For Moving Up
+        if(moveDir.y > 0)
+        {
+            animator.SetBool(MoveLeftAnim, false);
+            animator.SetBool(MoveRightAnim, false);
+            animator.SetBool(MoveUpAnim, true);
+        }
+        else if(moveDir.y == 0)
+        {
+            animator.SetBool(MoveUpAnim, false);
+        }
     }
 }
